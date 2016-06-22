4aff370b29a3007c443df498423c25dce7396d14
carlocas <carlocas@CARLOCAS2>
2016-06-22 10:45:50 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo-BlobStorage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ejemplo_BlobStorage.Startup))]
+namespace Ejemplo_BlobStorage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
